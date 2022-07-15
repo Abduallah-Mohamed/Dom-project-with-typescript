899592eb261acc0dbf023d5dc5ecebcf52d67115
Abduallah-Mohamed <Abduallahmohamed966@gmail.com>
2022-07-15 18:21:08 +0200

    finish the todo using typescript

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,48 @@
 "use strict";
-const btn = document.querySelector("button");
-// btn?.style.backgroundColor = "red";
-// console.log(btn);
-console.log("hello".replaceAll("l", "x"));
+const btn = document.getElementById("btn");
+const input = document.getElementById("todoInput");
+const form = document.getElementById("todoForm");
+const todoList = document.getElementById("todolist");
+const todos = readTodos();
+todos.forEach((todo) => createTodo(todo));
+function readTodos() {
+    const todos = localStorage.getItem("todos");
+    if (todos === null)
+        return [];
+    return JSON.parse(todos);
+    // console.log(localStorage.getItem("todos"));
+}
+function saveTodos() {
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
+function handleSubmit(e) {
+    e.preventDefault();
+    const todo = {
+        text: input.value,
+        completed: false,
+    };
+    todos.push(todo);
+    createTodo(todo);
+    saveTodos();
+    input.value = "";
+}
+function createTodo(todo) {
+    const li = document.createElement("li");
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.checked = todo.completed;
+    checkbox.addEventListener("change", function () {
+        todo.completed = checkbox.checked;
+        saveTodos();
+    });
+    li.appendChild(checkbox);
+    li.append(todo.text);
+    todoList.append(li);
+}
+form.addEventListener("submit", handleSubmit);
+// btn?.addEventListener("click", () => {
+//   // ? is optional
+//   console.log(input.value);
+//   input.value = "";
+//   input.autofocus = true;
+// });
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,58 @@
-const btn = document.querySelector("button");
-// btn?.style.backgroundColor = "red";
-// console.log(btn);
+interface Todos {
+  text: string;
+  completed: boolean;
+}
 
-console.log("hello".replaceAll("l", "x"));
+const btn = document.getElementById("btn")! as HTMLButtonElement;
+const input = document.getElementById("todoInput")! as HTMLInputElement;
+const form = document.getElementById("todoForm")!;
+const todoList = document.getElementById("todolist")!;
+const todos: Todos[] = readTodos();
+todos.forEach((todo) => createTodo(todo));
+
+function readTodos(): Todos[] {
+  const todos = localStorage.getItem("todos");
+  if (todos === null) return [];
+  return JSON.parse(todos);
+  // console.log(localStorage.getItem("todos"));
+}
+
+function saveTodos() {
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+function handleSubmit(e: SubmitEvent) {
+  e.preventDefault();
+  const todo: Todos = {
+    text: input.value,
+    completed: false,
+  };
+  todos.push(todo);
+  createTodo(todo);
+
+  saveTodos();
+  input.value = "";
+}
+
+function createTodo(todo: Todos) {
+  const li = document.createElement("li");
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.checked = todo.completed;
+  checkbox.addEventListener("change", function () {
+    todo.completed = checkbox.checked;
+    saveTodos();
+  });
+  li.appendChild(checkbox);
+  li.append(todo.text);
+  todoList.append(li);
+}
+
+form.addEventListener("submit", handleSubmit);
+
+// btn?.addEventListener("click", () => {
+//   // ? is optional
+//   console.log(input.value);
+//   input.value = "";
+//   input.autofocus = true;
+// });
